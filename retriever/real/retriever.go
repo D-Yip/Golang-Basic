a74ac2640ce927baf911e608c302da8f1e5c0615
YeSJ <yeshujin@outlook.com>
2019-03-27 01:27:13 +0800

    interface

--- retriever/real/retriever.go
+++ retriever/real/retriever.go
@@ -7,24 +7,19 @@ import (
 )
 
 type Retriever struct {
-	UserAgent string
-	TimeOut   time.Duration
+	UserAge string
+	TimeOut time.Duration
 }
 
-func (r *Retriever) Get(url string) string {
+func (r Retriever) Get(url string) string {
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
-
-	result, err := httputil.DumpResponse(
-		resp, true)
-
+	result, err := httputil.DumpResponse(resp, true)
 	resp.Body.Close()
-
 	if err != nil {
 		panic(err)
 	}
-
 	return string(result)
 }
