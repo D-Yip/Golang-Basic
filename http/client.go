ca3db402f3c7bf5d31d3a7b64e4a2f8e5ccb75eb
YeSJ <yeshuijin@outlook.com>
2019-04-09 10:47:27 +0800

    http client

--- http/client.go
+++ http/client.go
@@ -7,17 +7,13 @@ import (
 )
 
 func main() {
-	request, err := http.NewRequest(
-		http.MethodGet,
-		"http://www.imooc.com", nil)
-	request.Header.Add("User-Agent",
-		"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
 
+	request, err := http.NewRequest(http.MethodGet, "http://www.taobao.com", nil)
+	request.Header.Add("User-Agent",
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
 	client := http.Client{
-		CheckRedirect: func(
-			req *http.Request,
-			via []*http.Request) error {
-			fmt.Println("Redirect:", req)
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect : ", req)
 			return nil
 		},
 	}
